816cf191f27df5934d5daa00fbdf12e552e637f7
Michael Zhang <mail@mzhang.io>
2022-10-13 00:06:51 -0500

    Add triple_pin

--- README.md
+++ README.md
@@ -12,14 +12,17 @@ Python:
     - One way is by passing invalid UTF-8
     - Another way is to OOM the process
 
-- Command injection
+- Library
   - Solution: shell injection in the `show` command
+
+- In a Pickle
+  - Solution: send an eval pickle to the server to get RCE
 
 C:
 
-- Warmup
+- Triple Pin
   - Solution: view the assembly (or decompile) to find out what the process is
       doing
-
-- OBO
-  - Solution: overwrite a byte by taking advantage of off-by-one error
+    - First one is simple multiplication
+    - Second one is shift
+    - Third one is an LCG computed from the second
--- rev/first_c/main.c
+++ rev/first_c/main.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    int a, b, c;
-    printf("The pin is in 3 integers in the form 'xxx xxx xxx'\n");
-    printf("Enter the pin: ");
+void die() {
+  printf("Incorrect\n");
+  exit(0);
+}
 
-    scanf("%d %d %d", &a, &b, &c);
+int main(void) {
+  setvbuf(stdout, NULL, _IONBF, 0);
+
+  int a, b, c;
+  printf("The pin is in 3 integers in the form 'xxx xxx xxx'\n");
+  printf("Enter the pin: ");
+
+  scanf("%d %d %d", &a, &b, &c);
+
+  if (a * 24 != 3336)
+    die();
+
+  if (b << 12 != 1765376)
+    die();
+
+  int x = 123;
+  for (int i = 0; i < b; ++i) {
+    x = (x + 456 * b) % 999;
+  }
+  if (c != x)
+    die();
+
+  printf("The flag is %s\n", getenv("TRIPLE_PIN_FLAG"));
 }
