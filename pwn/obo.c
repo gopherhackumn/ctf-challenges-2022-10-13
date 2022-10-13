634e546ecb31941697aa8243a7fa928e392c12b3
Jaden Rodriguez <61155722+YaBoiSkinnyP@users.noreply.github.com>
2022-10-13 16:36:34 -0500

    Update obo.c

--- pwn/obo.c
+++ pwn/obo.c
@@ -2,16 +2,17 @@
 #include <unistd.h>
 
 int main() {
-    char name[128] = {0};
-    char flag[128] = {0};
+    length = 128;
+    char name[length] = {0};
+    char flag[length] = {0};
   
     // read in flag
     int file = fopen("flag.txt", "r");
-    fread(flag, 128, 1, file);
+    fread(flag, length, 1, file);
 
     // read in name
     printf("What's your name?\n");
-    read(0, name, 128);
+    read(0, name, length);
   
     printf("Hello: %s\n", name);
     printf("You'll never get my secret!\n");
