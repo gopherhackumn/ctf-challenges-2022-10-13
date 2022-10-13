9d59637efef35b59a2e82c53da9894495d5c8ff7
Michael Zhang <mail@mzhang.io>
2022-10-12 21:29:02 -0500

    aaa

---
+++ rev/rev1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+password = input("What is the password?")
+
+def reverse_string(s: str):
+    s_list = list(s)
+    s_list.reverse()
+    return "".join(s_list)
+
+if reverse_string(password) == "}e506e3cf0f7b8e432871f8dc03dc426c{rehpog":
+    print("You got it!")
+
+print("Nope.")
